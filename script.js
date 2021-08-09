b6c7b1def49d8759a00eb14d37d47b39bfc1af1d
Twealhula <needfreeblu@gmail.com>
2021-08-09 10:08:38 +0100

    add score tracker

--- script.js
+++ script.js
@@ -20,25 +20,38 @@ function playRound(playerSelection, computerSelection) { //take input of player 
 //Permutations for "Win" case: ROCK AND SCISSORS, SCISSORS AND PAPER, PAPER AND ROCK
 //Else return "Lose"
 
+//Print result in console, return 0, 1 or 2, each corresponding to Draw, Win or Lose
     if (x === y) {
-        return "It's a Draw!";
+        console.log("It's a Draw!")
+        return 0;
     }   else if (x === "rock" && y === "scissors" || x === "scissors" && y === "paper" || x === "paper" && y === "rock" ) {
-        return `You Win! ${x} beats ${y}`;
+        console.log(`You Win! ${x} beats ${y}`)
+        return 1;
     }   else {
-        return `You Lose! ${y} beats ${x}`;
+        console.log(`You Lose! ${y} beats ${x}`)
+        return 2;
     }
 }
 
 function game() { //call playRound 5 times and each time keep record of the score, return Win or Lose at the end
 
-    let playerScore = 0 //create player's score and set to 0
-    let computerScore = 0 //create computer's score and set to 0
+    let playerScore = 0; //create player's score and set to 0
+    let computerScore = 0; //create computer's score and set to 0
 
     for (let i = 0; i < 5; i++) { //loop code 5 times
-        let playerSelection = prompt("Rock Paper Scissors?")
+        let playerSelection = prompt("Rock Paper Scissors?");
         let computerSelection = computerPlay();
-        console.log(playRound(playerSelection, computerSelection))
-
+        
+        switch (playRound(playerSelection, computerSelection)) {
+            case 1:
+                ++playerScore;
+                break;
+            case 2:
+                ++computerScore;
+                break;
+        }
+    
+        console.log(`${playerScore} : ${computerScore}`)
     }
 
 }
